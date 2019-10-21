1b5dfab1c4b9d36a067191e545537bfc60cd0465
Hariprasad DJ <hari.p@spaneos.com>
2019-10-21 09:40:02 +0530

    Getting started

---
+++ src/main/java/com/heraizen/cj/git/Addition.java
@@ -0,0 +1,11 @@
+package com.heraizen.cj.git;
+
+public class Addition 
+{
+	
+	int add(int a,int b)
+			{
+		return a+b;
+	}
+
+}
---
+++ src/test/java/com/heraizen/cj/git/Testing.java
@@ -0,0 +1,12 @@
+package com.heraizen.cj.git;
+
+import org.junit.jupiter.api.Test;
+
+public class Testing {
+Addition a= new Addition();
+@Test
+public  void test() {
+	
+}
+
+}
